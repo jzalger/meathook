a9a82a16f57f42ac8a55d0ab483dda298117b4dc
jzalger <gmail.com>
2020-05-16 22:32:03 -0400

    logging query bug fixes

--- meathook_web/data_logging.py
+++ meathook_web/data_logging.py
@@ -12,25 +12,25 @@ poll_frequency = 60  # Frequency in seconds
 
 def query_device(did, variable):
     r = requests.get(api_get_url.substitute(dict(device_id=did, var_name=variable)),
-                     data=dict(access_token=particle_token))
+                     params=dict(access_token=particle_token))
     return r.json()
 
 
 def insert_data(new_data):
+    # TODO: Implement
     client = InfluxDBClient(host=influx_host, port=influx_port)
     client.switch_database(influx_db_name)
     client.write_points(new_data)
 
 
 def main():
-    variables = ["external_temp, fridge_temp, fridge_rh, fridge_temp_setpoint,"
-                 "fridge_state, fridge_rh_setpoint"]
+    variables = ["external_temp", "fridge_temp", "fridge_rh", "fridge_temp_setpoint",
+                 "fridge_state", "fridge_rh_setpoint"]
     # while True:
     for variable in variables:
         data = query_device(device_id, variable)
-        print(data)
-        # insert_data(data)
-        time.sleep(1)  # Rate limit the API queries
+        insert_data(data)
+        time.sleep(10)  # Rate limit the API queries
     time.sleep(poll_frequency)
 
 
