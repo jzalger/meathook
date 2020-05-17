6dfc530b82d14ab8b8418af1f233b2d9379a31fb
jzalger <gmail.com>
2020-05-17 11:05:05 -0400

    Functional data logging to influxdb

--- meathook_web/data_logging.py
+++ meathook_web/data_logging.py
@@ -7,17 +7,20 @@ from secrets import device_id, particle_token, influx_db_name, influx_host, infl
 api_get_url = Template("https://api.particle.io/v1/devices/$device_id/$var_name")
 api_vitals_url = Template("https://api.particle.io/v1/diagnostics/$device_id/last")
 
-poll_frequency = 60  # Frequency in seconds
+poll_frequency = 300  # Frequency in seconds
 
 
 def query_device(did, variable):
     r = requests.get(api_get_url.substitute(dict(device_id=did, var_name=variable)),
                      params=dict(access_token=particle_token))
-    return r.json()
+    if r.status_code == requests.codes.ok:
+        return r.json()
+    else:
+        raise requests.exceptions.RequestException
 
 
 def insert_data(new_data):
-    point = {"measurement":new_data[], "fields":{"value":new_data[]}, "tags":{}}
+    point = [{"measurement": new_data["name"], "fields": {"value": new_data["result"]}}]
     client = InfluxDBClient(influx_host, influx_port, influx_user, influx_password, influx_db_name)
     client.write_points(point)
 
@@ -25,12 +28,15 @@ def insert_data(new_data):
 def main():
     variables = ["external_temp", "fridge_temp", "fridge_rh", "fridge_temp_setpoint",
                  "fridge_state", "fridge_rh_setpoint"]
-    # while True:
-    for variable in variables:
-        data = query_device(device_id, variable)
-        insert_data(data)
-        time.sleep(10)  # Rate limit the API queries
-    time.sleep(poll_frequency)
+    while True:
+        for variable in variables:
+            try:
+                data = query_device(device_id, variable)
+                insert_data(data)
+            except requests.exceptions.RequestException:
+                pass
+            time.sleep(1)  # Rate limit the API queries
+        time.sleep(poll_frequency)
 
 
 if __name__ == "__main__":
