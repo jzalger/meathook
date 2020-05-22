49bd52c3d9f2e1ee9ababc974eec3fbae036d79e
jzalger <gmail.com>
2020-05-22 16:00:51 -0400

    Frontend updates

--- meathook_web/meathook.py
+++ meathook_web/meathook.py
@@ -5,7 +5,7 @@ meathook = Flask(__name__)
 def get_state():
     return dict(current_temp=8.5, current_rh=46, current_ext_temp=30,
                 temp_alert=False, rh_alert=False, door_alert=False,
-                temp_setpoint=8.6, rh_setpoint=45)
+                temp_setpoint=8.6, rh_setpoint=45, temp_alarm_delta=10, rh_alarm_delta=20)
 
 
 @meathook.route('/')
