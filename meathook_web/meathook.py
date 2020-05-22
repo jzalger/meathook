c2ad21073af8743371b30d144d4bdb45105b719e
jzalger <gmail.com>
2020-05-22 12:06:08 -0400

    Frontend updates with server side rendering of state Refactored css and template Minor backend updates

--- meathook_web/meathook.py
+++ meathook_web/meathook.py
@@ -2,9 +2,16 @@ from flask import Flask, render_template
 meathook = Flask(__name__)
 
 
+def get_state():
+    return dict(current_temp=8.5, current_rh=46, current_ext_temp=30,
+                temp_alert=False, rh_alert=False, door_alert=False,
+                temp_setpoint=8.6, rh_setpoint=45)
+
+
 @meathook.route('/')
 def main():
-    return render_template('meathook.html')
+    state = get_state()
+    return render_template('meathook.html', state=state)
 
 
 @meathook.route('/current-temp')
@@ -12,7 +19,7 @@ def current_temp():
     return 0
 
 
-@meathook.route('current-rh')
+@meathook.route('/current-rh')
 def current_rh():
     return 0.0
 
