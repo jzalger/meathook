ad8a871f42acf30b31833bdbc4bf23d64f325c24
Jonathan Zalger <jzalger@users.noreply.github.com>
2022-08-09 19:20:45 -0400

    Expanded unit tests

---
+++ tests/unit/test_meathook.py
@@ -0,0 +1,71 @@
+"""
+Unit Tests for meathook.py
+"""
+import tempfile
+import meathook
+from unittest import TestCase, skip, mock
+
+
+class TestMeathook(TestCase):
+
+    def test_init(self):
+        self.assertEqual(, )
+
+    def test_start_stream(self):
+        pass
+
+    def test_stop_stream(self):
+        pass
+
+    def test__start_stream(self):
+        pass
+        
+    def test_handle_state_update(self):
+        pass
+        
+    def test__get_variable(self):
+        pass
+        
+    def test_get_state(self):
+        pass
+    
+    def test_is_online(self):
+        pass
+        
+    def test_set_led_state(self):
+        pass
+        
+    def test_set_temp_setpoint(self):
+        pass
+        
+    def test_set_fan_state(self):
+        pass
+        
+    def test_set_fridge_state(self):
+        pass
+        
+    def test_set_temp_control(self):
+        pass
+        
+    def test_set_temp_alarm_delta(self):
+        pass
+        
+    def test_set_rh_alarm_limit(self):
+        pass
+        
+    def test_set_control_alg(self):
+        pass
+        
+    def test_set_es_state(self):
+        pass
+        
+    def test_set_es_temp_setpoint(self):
+        pass
+        
+    def test_set_es_timing(self):
+        pass
+        
+    def test__call_func(self):
+        pass
+        
+        
