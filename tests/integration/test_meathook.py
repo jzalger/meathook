fafee318e00c897eff77aa36a0cf428b7527c5e2
jzalger <jzalger@users.noreply.github.com>
2022-08-10 20:13:34 -0400

    test updates

--- tests/integration/test_meathook.py
+++ tests/integration/test_meathook.py
@@ -7,46 +7,46 @@ import meathook_web.meathook as meathook
 
 
 def test__start_stream():
-    pytest.skip("To be implemented")
+    pass
 
 def test__get_variable():
-    pytest.skip("To be implemented")
+    pass
 
 def test_is_online():
-    pytest.skip("To be implemented")
+    pass
     
 def test_set_led_state():
-    pytest.skip("To be implemented")
+    pass
     
 def test_set_temp_setpoint():
-    pytest.skip("To be implemented")
+    pass
     
 def test_set_fan_state():
-    pytest.skip("To be implemented")
+    pass
     
 def test_set_fridge_state():
-    pytest.skip("To be implemented")
+    pass
     
 def test_set_temp_control():
-    pytest.skip("To be implemented")
+    pass
     
 def test_set_temp_alarm_delta():
-    pytest.skip("To be implemented")
+    pass
     
 def test_set_rh_alarm_limit():
-    pytest.skip("To be implemented")
+    pass
     
 def test_set_control_alg():
-    pytest.skip("To be implemented")
+    pass
     
 def test_set_es_state():
-    pytest.skip("To be implemented")
+    pass
     
 def test_set_es_temp_setpoint():
-    pytest.skip("To be implemented")
+    pass
     
 def test_set_es_timing():
-    pytest.skip("To be implemented")
+    pass
     
 def test__call_func():
-    pytest.skip("To be implemented")
+    pass
--- tests/integration/test_meathook_web.py
+++ tests/integration/test_meathook_web.py
@@ -17,38 +17,38 @@ def test_get_device_state(test_client):
     assert b'{}\n' in response.data
 
 def test_get_var_state():
-    pytest.skip("To be implemented")
+    pass
     
 def test_set_led_state():
-    pytest.skip("To be implemented")
+    pass
     
 def test_set_temp_control():
-    pytest.skip("To be implemented")
+    pass
     
 def test_set_fan_state():
-    pytest.skip("To be implemented")
+    pass
     
 def test_set_temp_setpoint():
-    pytest.skip("To be implemented")
+    pass
     
 def test_set_ctl_algorithm():
-    pytest.skip("To be implemented")
+    pass
     
 def test_set_fridge_state():
-    pytest.skip("To be implemented")
+    pass
     
 def test_set_temp_alarm_threashold():
-    pytest.skip("To be implemented")
+    pass
     
 def test_set_rh_alarm_limit():
-    pytest.skip("To be implemented")
+    pass
     
 def test_set_es_state():
-    pytest.skip("To be implemented")
+    pass
     
 def test_set_es_temp_setpoint():
-    pytest.skip("To be implemented")
+    pass
     
 def test_set_es_timing():
-    pytest.skip("To be implemented")
+    pass
 
